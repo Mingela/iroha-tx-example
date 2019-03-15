f64087831aea7d80143d8e42575d9745b16f5bc6
Bogdan Mingela <freekilluau@gmail.com>
2019-03-15 12:32:45 +0300

    Added test on qurying newly created account

--- src/test/kotlin/example/SinglePeerAccountCreationTest.kt
+++ src/test/kotlin/example/SinglePeerAccountCreationTest.kt
@@ -1,6 +1,9 @@
 package example
 
 import iroha.protocol.Endpoint.TxStatus
+import jp.co.soramitsu.iroha.java.BlocksQueryBuilder
+import jp.co.soramitsu.iroha.java.Query
+import jp.co.soramitsu.iroha.java.Transaction
 import jp.co.soramitsu.iroha.java.Utils
 import jp.co.soramitsu.iroha.testcontainers.IrohaContainer
 import jp.co.soramitsu.iroha.testcontainers.detail.GenesisBlockBuilder
@@ -9,6 +12,9 @@ import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions.assertNotEquals
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import java.time.Instant
+import java.util.*
+import java.util.concurrent.atomic.AtomicLong
 
 class SinglePeerAccountCreationTest {
 
@@ -65,5 +71,48 @@ class SinglePeerAccountCreationTest {
         }
     }
 
-    companion object : KLogging()
+    @Test
+    fun worldStateViewUpdating() {
+        val api = iroha.api
+        val counter = AtomicLong(1)
+        val query = BlocksQueryBuilder(GenesisBlockBuilder.defaultAccountId, Instant.now(), counter.getAndIncrement())
+                .buildSigned(GenesisBlockBuilder.defaultKeyPair)
+        api.blocksQuery(query).subscribe {
+            val payload = it.blockResponse.block.blockV1.payload
+            println("Got block #${payload.height}. Let's query the account")
+            // try to play with this value
+            Thread.sleep(1)
+            val createAccount = payload.transactionsList[0].payload.reducedPayload.commandsList[0].createAccount
+            val queryResponse = api.query(Query.builder(GenesisBlockBuilder.defaultAccountId, counter.getAndIncrement())
+                    .getAccount(createAccount.accountName + "@" + createAccount.domainId)
+                    .buildSigned(GenesisBlockBuilder.defaultKeyPair)
+            )
+            println("Got the reply:\n$queryResponse")
+            if (queryResponse.hasErrorResponse()) {
+                System.exit(1)
+            }
+        }
+        while (true) {
+            Thread.sleep(500)
+            api.transactionSync(Transaction.builder(GenesisBlockBuilder.defaultAccountId)
+                    .createAccount(String.getRandomString(10), GenesisBlockBuilder.defaultDomainName, GenesisBlockBuilder.defaultKeyPair.public)
+                    .sign(GenesisBlockBuilder.defaultKeyPair)
+                    .build()
+            )
+        }
+    }
+
+    /** Returns random string of [len] characters */
+    fun String.Companion.getRandomString(len: Int): String {
+        val random = Random()
+        val res = StringBuilder()
+        for (i in 0..len) {
+            res.append(CHAR[random.nextInt(CHAR.length)])
+        }
+        return res.toString()
+    }
+
+    companion object : KLogging() {
+        private const val CHAR = "abcdefghijklmnopqrstuvwxyz"
+    }
 }
