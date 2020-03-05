137e8f4b64b87cfadae2f05d90e5ae9fe5017f98
Mingela <freekilluau@gmail.com>
2020-03-05 08:48:51 +0300

    Updated tests

--- src/main/kotlin/example/AccountCreatorHelper.kt
+++ src/main/kotlin/example/AccountCreatorHelper.kt
@@ -10,8 +10,8 @@ import jp.co.soramitsu.iroha.testcontainers.detail.GenesisBlockBuilder
 
 object AccountCreatorHelper {
 
-    private val timeOffset = 1000L
-    val txList = mutableListOf<TransactionOuterClass.Transaction>()
+    private const val timeOffset = 1000L
+    private val txList = mutableListOf<TransactionOuterClass.Transaction>()
 
     fun getRolesTransactions(mortalAccount: String): List<TransactionOuterClass.Transaction> {
         var time = System.currentTimeMillis() - timeOffset * 20
@@ -19,100 +19,124 @@ object AccountCreatorHelper {
         val newKeypair = Ed25519Sha3().generateKeypair()
 
         // Create empty role for domain
-        txList.add(TransactionBuilder(GenesisBlockBuilder.defaultAccountId, time)
+        txList.add(
+            TransactionBuilder(GenesisBlockBuilder.defaultAccountId, time)
                 .setQuorum(1)
                 .createRole("none", emptyList())
                 .sign(GenesisBlockBuilder.defaultKeyPair)
-                .build())
+                .build()
+        )
         time += timeOffset
         // Create domain
-        txList.add(TransactionBuilder(GenesisBlockBuilder.defaultAccountId, time)
+        txList.add(
+            TransactionBuilder(GenesisBlockBuilder.defaultAccountId, time)
                 .setQuorum(1)
                 .createDomain("soramitsu", "none")
                 .sign(GenesisBlockBuilder.defaultKeyPair)
-                .build())
+                .build()
+        )
         time += timeOffset
         // Create account
-        txList.add(TransactionBuilder(GenesisBlockBuilder.defaultAccountId, time)
+        txList.add(
+            TransactionBuilder(GenesisBlockBuilder.defaultAccountId, time)
                 .setQuorum(1)
                 .createAccount(fullAccountName, newKeypair.public)
                 .sign(GenesisBlockBuilder.defaultKeyPair)
-                .build())
+                .build()
+        )
         time += timeOffset
         // Create first role
-        txList.add(TransactionBuilder(GenesisBlockBuilder.defaultAccountId, time)
+        txList.add(
+            TransactionBuilder(GenesisBlockBuilder.defaultAccountId, time)
                 .setQuorum(1)
                 .createRole("god_of_details", listOf(Primitive.RolePermission.can_set_detail))
                 .sign(GenesisBlockBuilder.defaultKeyPair)
-                .build())
+                .build()
+        )
         time += timeOffset
         // Create second role
-        txList.add(TransactionBuilder(GenesisBlockBuilder.defaultAccountId, time)
+        txList.add(
+            TransactionBuilder(GenesisBlockBuilder.defaultAccountId, time)
                 .setQuorum(1)
                 .createRole("god_of_roles", listOf(Primitive.RolePermission.can_append_role))
                 .sign(GenesisBlockBuilder.defaultKeyPair)
-                .build())
+                .build()
+        )
         time += timeOffset
         // Append second role
-        txList.add(TransactionBuilder(GenesisBlockBuilder.defaultAccountId, time)
+        txList.add(
+            TransactionBuilder(GenesisBlockBuilder.defaultAccountId, time)
                 .setQuorum(1)
                 .appendRole(fullAccountName, "god_of_roles")
                 .sign(GenesisBlockBuilder.defaultKeyPair)
-                .build())
+                .build()
+        )
         time += timeOffset
         // Append first role by self
-        txList.add(TransactionBuilder(fullAccountName, time)
+        txList.add(
+            TransactionBuilder(fullAccountName, time)
                 .setQuorum(1)
                 .appendRole(fullAccountName, "god_of_details")
                 .sign(newKeypair)
-                .build())
+                .build()
+        )
         time += timeOffset
         // Check first role
-        txList.add(TransactionBuilder(fullAccountName, time)
+        txList.add(
+            TransactionBuilder(fullAccountName, time)
                 .setQuorum(1)
                 .setAccountDetail(fullAccountName, "key", "value")
                 .setAccountDetail(fullAccountName, "key1", "value2")
                 .setAccountDetail(fullAccountName, "key2", "value3")
                 .sign(newKeypair)
-                .build())
+                .build()
+        )
 
         return txList
     }
 
-    fun getBatchTransactions(mortalAccount: String): List<TransactionOuterClass.Transaction> {
+    fun getBatchTransactions(mortalAccount: String): MutableList<TransactionOuterClass.Transaction> {
         var time = System.currentTimeMillis() - timeOffset * 20
         val fullAccountName = "$mortalAccount@soramitsu"
         val newKeypair = Ed25519Sha3().generateKeypair()
 
         // Create empty role for domain
-        txList.add(TransactionBuilder(GenesisBlockBuilder.defaultAccountId, time)
+        txList.add(
+            TransactionBuilder(GenesisBlockBuilder.defaultAccountId, time)
                 .setQuorum(1)
                 .createRole("none", emptyList())
                 .sign(GenesisBlockBuilder.defaultKeyPair)
-                .build())
+                .build()
+        )
         time += timeOffset
         // Create domain
-        txList.add(TransactionBuilder(GenesisBlockBuilder.defaultAccountId, time)
+        txList.add(
+            TransactionBuilder(GenesisBlockBuilder.defaultAccountId, time)
                 .setQuorum(1)
                 .createDomain("soramitsu", "none")
                 .sign(GenesisBlockBuilder.defaultKeyPair)
-                .build())
+                .build()
+        )
         time += timeOffset
         // Create account
-        txList.add(TransactionBuilder(GenesisBlockBuilder.defaultAccountId, time)
+        txList.add(
+            TransactionBuilder(GenesisBlockBuilder.defaultAccountId, time)
                 .setQuorum(1)
                 .createAccount(fullAccountName, newKeypair.public)
                 .sign(GenesisBlockBuilder.defaultKeyPair)
-                .build())
+                .build()
+        )
         time += timeOffset
         // Write something
-        txList.add(TransactionBuilder(GenesisBlockBuilder.defaultAccountId, time)
+        txList.add(
+            TransactionBuilder(GenesisBlockBuilder.defaultAccountId, time)
                 .setQuorum(1)
                 .setAccountDetail(fullAccountName + "abc", "key", "value")
                 .setAccountDetail(fullAccountName, "key1", "value2")
                 .setAccountDetail(fullAccountName, "key2", "value3")
                 .sign(GenesisBlockBuilder.defaultKeyPair)
-                .build())
+                .build()
+        )
 
         return txList
     }
@@ -120,7 +144,7 @@ object AccountCreatorHelper {
     fun getQuery(accountName: String): Queries.Query {
         // Query account roles
         return QueryBuilder(GenesisBlockBuilder.defaultAccountId, System.currentTimeMillis(), 1)
-                .getAccount("$accountName@soramitsu")
-                .buildSigned(GenesisBlockBuilder.defaultKeyPair)
+            .getAccount("$accountName@soramitsu")
+            .buildSigned(GenesisBlockBuilder.defaultKeyPair)
     }
 }
--- src/test/kotlin/example/SinglePeerAccountCreationTest.kt
+++ src/test/kotlin/example/SinglePeerAccountCreationTest.kt
@@ -1,12 +1,17 @@
 package example
 
+import com.google.gson.Gson
 import iroha.protocol.Endpoint.TxStatus
+import iroha.protocol.TransactionOuterClass
+import jp.co.soramitsu.crypto.ed25519.Ed25519Sha3
 import jp.co.soramitsu.iroha.java.BlocksQueryBuilder
 import jp.co.soramitsu.iroha.java.Query
 import jp.co.soramitsu.iroha.java.Transaction
 import jp.co.soramitsu.iroha.java.Utils
 import jp.co.soramitsu.iroha.testcontainers.IrohaContainer
 import jp.co.soramitsu.iroha.testcontainers.detail.GenesisBlockBuilder
+import jp.co.soramitsu.iroha.testcontainers.detail.GenesisBlockBuilder.defaultKeyPair
+import jp.co.soramitsu.iroha.testcontainers.detail.GenesisBlockBuilder.defaultRoleName
 import mu.KLogging
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions.assertNotEquals
@@ -138,6 +143,226 @@ class SinglePeerAccountCreationTest {
                         "key"
                     )
                     .buildSigned(GenesisBlockBuilder.defaultKeyPair)
+            )
+        )
+    }
+
+    @Test
+    fun testBatchSignatories() {
+        val api = iroha.api
+        val tx1 = Transaction.builder(GenesisBlockBuilder.defaultAccountId)
+            .setAccountDetail(GenesisBlockBuilder.defaultAccountId, "key1", "v1")
+            .build()
+        val tx2 = Transaction.builder(GenesisBlockBuilder.defaultAccountId)
+            .setAccountDetail(GenesisBlockBuilder.defaultAccountId, "key1", "v2")
+            .build()
+
+        val batch = Utils.createTxUnsignedAtomicBatch(mutableListOf(tx1, tx2))
+        val iterator = batch.iterator()
+        val txx1 = iterator.next()
+        val txx2 = iterator.next()
+        val trueBatch = listOf<TransactionOuterClass.Transaction>(
+            txx1.sign(GenesisBlockBuilder.defaultKeyPair).build(),
+            txx2.build()
+        )
+        api.transactionListSync(trueBatch)
+        Thread.sleep(5000)
+        val trueBatch2 = listOf<TransactionOuterClass.Transaction>(
+            trueBatch[0],
+            txx2.sign(GenesisBlockBuilder.defaultKeyPair).build()
+        )
+        api.transactionListSync(trueBatch2)
+        Thread.sleep(10000)
+        trueBatch.forEach {
+            val hash = Utils.hash(it)
+            val txStatusSync = api.txStatusSync(hash)
+            println(txStatusSync)
+            assertNotEquals(TxStatus.STATEFUL_VALIDATION_FAILED, txStatusSync.txStatus)
+        }
+    }
+
+    @Test
+    fun testImmediateDetails() {
+        println(
+            iroha.api.transaction(
+                Transaction.builder(GenesisBlockBuilder.defaultAccountId)
+                    .setAccountDetail(
+                        GenesisBlockBuilder.defaultAccountId,
+                        "key",
+                        String.getRandomString(10)
+                    )
+                    .sign(GenesisBlockBuilder.defaultKeyPair)
+                    .build()
+            ).blockingLast()
+        )
+
+        println(
+            iroha.api.query(
+                Query.builder(GenesisBlockBuilder.defaultAccountId, 1)
+                    .disableValidation()
+                    .getAccountDetail(
+                        GenesisBlockBuilder.defaultAccountId,
+                        GenesisBlockBuilder.defaultAccountId,
+                        "key"
+                    )
+                    .buildSigned(GenesisBlockBuilder.defaultKeyPair)
+            )
+        )
+    }
+
+    @Test
+    fun testDetailsOfOther() {
+        println(
+            iroha.api.transaction(
+                Transaction.builder(GenesisBlockBuilder.defaultAccountId)
+                    .createAccount("other", GenesisBlockBuilder.defaultDomainName, "".toByteArray())
+                    .setAccountDetail(
+                        GenesisBlockBuilder.defaultAccountId,
+                        "key",
+                        String.getRandomString(10)
+                    )
+                    .sign(GenesisBlockBuilder.defaultKeyPair)
+                    .build()
+            ).blockingLast()
+        )
+
+        println(
+            iroha.api.query(
+                Query.builder(GenesisBlockBuilder.defaultAccountId, 1)
+                    .disableValidation()
+                    .getAccountDetail(
+                        GenesisBlockBuilder.defaultAccountId,
+                        GenesisBlockBuilder.defaultAccountId,
+                        "key"
+                    )
+                    .buildSigned(GenesisBlockBuilder.defaultKeyPair)
+            )
+        )
+    }
+
+    @Test
+    fun testCAS() {
+        repeat(50) {
+            println("////////////////////////////////////////////////////////////////////////////////////////////////////")
+            val key = String.getRandomString(10)
+            Thread(
+                Runnable {
+                    println(
+                        "1" +
+                                iroha.api.transaction(
+                                    Transaction.builder(GenesisBlockBuilder.defaultAccountId)
+                                        .compareAndSetAccountDetail(
+                                            GenesisBlockBuilder.defaultAccountId,
+                                            key,
+                                            "123",
+                                            null
+                                        )
+                                        .sign(GenesisBlockBuilder.defaultKeyPair)
+                                        .build()
+                                ).blockingLast()
+                    )
+                    println(
+                        "1" +
+                                iroha.api.query(
+                                    Query.builder(GenesisBlockBuilder.defaultAccountId, (it * 2 + 1).toLong())
+                                        .getAccountDetail(
+                                            GenesisBlockBuilder.defaultAccountId,
+                                            GenesisBlockBuilder.defaultAccountId,
+                                            key
+                                        ).buildSigned(GenesisBlockBuilder.defaultKeyPair)
+                                )
+                    )
+                }
+            ).start()
+            Thread(
+                Runnable {
+                    println(
+                        "2" +
+                                iroha.api.transaction(
+                                    Transaction.builder(GenesisBlockBuilder.defaultAccountId)
+                                        .compareAndSetAccountDetail(
+                                            GenesisBlockBuilder.defaultAccountId,
+                                            key,
+                                            "456",
+                                            null
+                                        )
+                                        .sign(GenesisBlockBuilder.defaultKeyPair)
+                                        .build()
+                                ).blockingLast()
+                    )
+                    println(
+                        "2" +
+                                iroha.api.query(
+                                    Query.builder(GenesisBlockBuilder.defaultAccountId, (it * 2 + 2).toLong())
+                                        .getAccountDetail(
+                                            GenesisBlockBuilder.defaultAccountId,
+                                            GenesisBlockBuilder.defaultAccountId,
+                                            key
+                                        ).buildSigned(GenesisBlockBuilder.defaultKeyPair)
+                                )
+                    )
+                }
+            ).start()
+            Thread.sleep(2500)
+        }
+    }
+
+    @Test
+    fun testListJSONDetails() {
+        val gson = Gson()
+        val hex1 = Utils.toHex(Ed25519Sha3().generateKeypair().public.encoded)
+        val hex2 = Utils.toHex(Ed25519Sha3().generateKeypair().public.encoded)
+        val str = Utils.irohaEscape(gson.toJson(listOf(hex1, hex2)))
+        println(
+            iroha.api.transaction(
+                Transaction.builder(GenesisBlockBuilder.defaultAccountId)
+                    .setAccountDetail(
+                        GenesisBlockBuilder.defaultAccountId,
+                        "key",
+                        str
+                    )
+                    .sign(GenesisBlockBuilder.defaultKeyPair)
+                    .build()
+            ).blockingLast()
+        )
+
+        println(
+            iroha.api.transaction(
+                Transaction.builder(GenesisBlockBuilder.defaultAccountId)
+                    .setAccountDetail(
+                        GenesisBlockBuilder.defaultAccountId,
+                        "key",
+                        str
+                    )
+                    .sign(GenesisBlockBuilder.defaultKeyPair)
+                    .build()
+            ).blockingLast()
+        )
+    }
+
+    @Test
+    fun testQuery() {
+        val keyPair = Ed25519Sha3().generateKeypair()
+        val domainName = "ImpiloLogTest"
+        val accountName = "test2"
+        val accountId = "$accountName@$domainName"
+        val assetName = "login_attempt"
+        val assetId = "$assetName#$domainName"
+        println(
+            iroha.api.transaction(
+                Transaction.builder(GenesisBlockBuilder.defaultAccountId)
+                    .createDomain(domainName, defaultRoleName)
+                    .createAccount(accountName, domainName, keyPair.public)
+                    .createAsset(assetName, domainName, 0)
+                    .sign(defaultKeyPair).build()
+            ).blockingLast()
+        )
+
+        println(
+            iroha.api.query(
+                Query.builder(accountId, 1)
+                    .getAccountAssetTransactions(accountId, assetId, 5, null)
+                    .buildSigned(keyPair)
             )
         )
     }
